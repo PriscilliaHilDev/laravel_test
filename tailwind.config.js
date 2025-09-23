cef47e5f3cfc723cbd5777fdb5e114c15962f147
PriscilliaHilDev <priscillia.hillion@outlook.fr>
2025-09-23 13:46:30 -0400

    Install Jetstream with Livewire and setup authentication

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#1E40AF',
+                secondary: '#9333EA',
+            },Ò
+        },
+    },
+
+    plugins: [forms, typography],
+};
