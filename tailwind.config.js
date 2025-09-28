fdff9bacc830ebd94a936066f725c66d7e1c3828
PriscilliaHilDev <priscillia.hillion@outlook.fr>
2025-09-28 19:47:06 -0400

    install and configure TallStackUI with prefixed components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,17 @@ import flowbite from "flowbite/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+     presets: [ 
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js') 
+    ],
     darkMode: 'media', // ou 'media' si tu veux que ça suive les préférences système
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-         "./node_modules/flowbite/**/*.js"
+         "./node_modules/flowbite/**/*.js",
+        './vendor/tallstackui/tallstackui/src/**/*.php', 
     ],
 
     theme: {
